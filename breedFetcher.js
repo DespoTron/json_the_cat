0259622116a1eb7ce5996188a0f25db9e405ff53
Thai Do <thai.hl.do@gmail.com>
2020-08-27 12:28:45 +0000

    refactored breedFetcher.js to remove console logs and use callbacks

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,22 +1,20 @@
 const request = require('request');
 const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
 const breedNames = process.argv.slice(2);
-const urlSearchBreedName = url + breedNames;
 
-request(urlSearchBreedName, (error, response, body) => {
-  if (error) {
-    console.log('Wrong URL Entered', error);
-  }
+const fetchBreedDescription = function(breedName, callback) {
+  request(url + breedName, (error, response, body) => {
+    const data = JSON.parse(body);
+    const breedName = data[0];
 
-  //console.log('statusCode:', response && response.statusCode);
-  //console.log('Body:', typeof body);
-  const data = JSON.parse(body);
-  const breedName = data[0]
-  if(breedName === undefined) {
-    console.log('Error unknown Cat!');
-  } else {
-    console.log(breedName.description);
-    console.log(typeof data);
-  }
+    if (error) {
+      callback('Wrong URL Entered', error);
+    } else if (breedName === undefined) {
+      callback('Error unknown Cat!', );
+    } else {
+      callback(null, breedName.description);
+    }
+  });
+};
 
-});
+module.exports = { fetchBreedDescription };
