aa94e5e216a4e336cccf82b309747526a5185eb9
Thai Do <thai.hl.do@gmail.com>
2020-08-27 11:26:36 +0000

    tested edge cases for breed name and url if they don't work

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,15 +1,22 @@
 const request = require('request');
-const url = 'https://api.thecatapi.com/v1/breeds/search?q='
-let breedNames = process.argv.slice(2);
+const url = 'https://api.thecatapi.com/v1/breeds/search?q=';
+const breedNames = process.argv.slice(2);
+const urlSearchBreedName = url + breedNames;
 
-request(url + breedNames, (error, response, body) => { 
-  if(error) { 
-    console.log('Error:', error); 
-}
- console.log('statusCode:', response && response.statusCode); 
- console.log('Body:', typeof body) 
- const data = JSON.parse(body); 
- console.log(data[0].description); 
- console.log(typeof data);
+request(urlSearchBreedName, (error, response, body) => {
+  if (error) {
+    console.log('Wrong URL Entered', error);
+  }
+
+  //console.log('statusCode:', response && response.statusCode);
+  //console.log('Body:', typeof body);
+  const data = JSON.parse(body);
+  const breedName = data[0]
+  if(breedName === undefined) {
+    console.log('Error unknown Cat!');
+  } else {
+    console.log(breedName.description);
+    console.log(typeof data);
+  }
 
 });
